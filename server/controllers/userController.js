9f32cb4fabef7aa6dd2438851b95b85ff4c8b6c4
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-03-20 09:21:36 +0200

    add routes, models and controllers

---
+++ server/controllers/userController.js
@@ -0,0 +1,61 @@
+const ApiError = require('../error/ApiError');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {User, Basket} = require('../models/models');
+
+const generateJWT = (id, email, role) => {
+    return jwt.sign(
+        {id, email, role}, 
+    process.env.SECRET_KEY,
+        { expiresIn: '24h' }
+)
+}
+
+
+class UserController {
+    async registration(req, res, next) {  
+        const {email, password, role} = req.body
+        if (!email ||!password) {
+            return next(ApiError.badRequest('Некорректный email или пароль'))
+        }
+        const candidate = await User.findOne({ where: { email } });
+        if (candidate) {
+            return next(ApiError.badRequest('Такой пользователь уже существует'))
+        }
+        const hashedPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({
+            email,
+            password: hashedPassword,
+            role
+        })
+        const basket = await Basket.create({
+            userId: user.id
+        })
+        const token = generateJWT(user.id, user.email, user.role)
+        return res.json({token})
+
+    }  
+
+    async login(req, res) { 
+        // const { email, password } = req.body;
+        // const user = await User.findOne({ where: { email } });
+        // if (!user) {
+        //     return res.status(401).json({ error: 'User not found' });
+        // }
+        // const isPasswordValid = await user.checkPassword(password);
+        // if (!isPasswordValid) {
+        //     return res.status(401).json({ error: 'Invalid password' });
+        // }
+        // const { id, name, email } = user;
+        // return res.json({ id, name, email });
+    }
+    async check(req, res, next) {
+        const {id} = req.query
+        if (!id) { 
+            return next(ApiError.badRequest('Не задан ID'))
+        }
+        res.json(id)
+        }
+}
+
+module.exports = new UserController()
--- server/models/models.js
+++ server/models/models.js
@@ -70,7 +70,7 @@ Rating.belongsTo(Device)
 Device.hasMany(BasketDevice)
 BasketDevice.belongsTo(Device)
 
-Device.hasMany(DeviceInfo)
+Device.hasMany(DeviceInfo, {as:'info'})
 DeviceInfo.belongsTo(Device)
 
 Type.belongsToMany(Brand, {through: TypeBrand})
@@ -86,5 +86,4 @@ module.exports = {
     Rating,
     DeviceInfo,
     TypeBrand
-
 }
